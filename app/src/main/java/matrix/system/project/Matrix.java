16994bcbcdb56f33a56ab287c7b3cd43f4d4b30b
Matt Williams <mjawilliams39@gmail.com>
2026-05-10 01:22:47 +1200

    Finished Creation of Random Matricies and Formatting

--- app/src/main/java/matrix/system/project/Matrix.java
+++ app/src/main/java/matrix/system/project/Matrix.java
@@ -7,16 +7,42 @@ public class Matrix {
     public int rows;
     public int columns;
     double[][]matrix;
+    int decimalMax;
 
 
     public Matrix(int rows, int columns){
         this.rows = rows;
         this.columns = columns;
         this.matrix = new double[rows][columns];
+        this.decimalMax = 0;
+    }
+
+    public void fillRandomInt(int low,int high){
         Random rng = new Random();
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
-                matrix[i][j] = rng.nextDouble(10);
+                matrix[i][j] = rng.nextInt(low,high+1);
+            }
+        }
+    }
+
+    public void fillRandomDouble(int low,int high, int decimalMax){
+        if(decimalMax < 0){
+            System.out.println("Can't have a negative decimal");
+            return;
+        }
+        if(decimalMax == 0){
+            fillRandomInt(low,high);
+            this.decimalMax = 0;
+            return;
+        }
+        this.decimalMax = decimalMax;
+        DecimalFormat nf1 = new DecimalFormat();
+        nf1.setMaximumFractionDigits(decimalMax);
+        Random rng = new Random();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                matrix[i][j] = Double.parseDouble(nf1.format(rng.nextDouble(low,high)));
             }
         }
     }
@@ -33,14 +59,16 @@ public class Matrix {
     public String toString(){
         StringBuilder sb = new StringBuilder();
         DecimalFormat dmf = new DecimalFormat();
-        dmf.setMaximumFractionDigits(2);
+        dmf.setMaximumFractionDigits(decimalMax);
         for(int i=0;i<matrix.length;i++){
             sb.append("(");
             for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]%1 == 0){
                     sb.append((int)(matrix[i][j]));
-                } else {
+                } else if(decimalMax != 0){
                     sb.append(dmf.format(matrix[i][j]));
+                } else {
+                    sb.append(matrix[i][j]);
                 }
                 if(j != matrix[i].length-1){
                     sb.append(", ");
--- app/src/main/java/matrix/system/project/MatrixApp.java
+++ app/src/main/java/matrix/system/project/MatrixApp.java
@@ -3,8 +3,11 @@ package matrix.system.project;
 public class MatrixApp {
     public static void main(String[]args){
         Matrix matrix1 = new Matrix(3,3);
+        matrix1.fillRandomDouble(0,9,2);
         System.out.println(matrix1);
-
+        Matrix matrix2 = new Matrix(2,2);
+        matrix2.fillRandomInt(-5,5);
+        System.out.println(matrix2);
 
 
 
